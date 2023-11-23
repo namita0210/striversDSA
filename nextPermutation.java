b890c2786db97e6825d7a9f01d396aac6a0f1b0e
namita0210 <namita008mtaids22@igdtuw.ac.in>
2023-11-23 19:27:37 +0530

    swap function, reverse array optimal function

--- nextPermutation.java
+++ nextPermutation.java
@@ -14,6 +14,29 @@ public class nextPermutation {
         return pivot;
     }
 
+    //swap function
+    public static int[] swap(int x , int y , int[]arr){
+        int temp = 0;
+        temp = arr[x];
+        arr[x] = arr[y];
+        arr[y]=temp;
+        return arr;
+    }
+
+    //reverse an array - > Can be done with built in function Arrays.reverse()
+    public static int[] reverseArray(int[]arr){
+        int n = arr.length;
+        int si = 0;
+        int ei = n-1;
+        while (si<=ei ){
+            //swap array at index si with index ei
+            swap(si, ei, arr);
+            //Now increase the si and decrease the ei
+            si++; ei--;
+        }
+        return arr;
+    }
+
     //display an array
     public static void display(int[]arr){
         for(int i = 0 ; i < arr.length; i++){
@@ -32,6 +55,10 @@ public class nextPermutation {
     //Main Method
     public static void main(String[]args){
         int[]arr = {1,2,3};
+        display(arr);
+        reverseArray(arr);
+        System.out.println();
+        display(arr);
         
     }
 }
