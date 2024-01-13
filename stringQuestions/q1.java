943ba2ddbfb2e877bfe9095ae117cf47f72bc7a3
Namita <46557775+namita0210@users.noreply.github.com>
2024-01-13 16:41:39 +0000

    reverse string

---
+++ stringQuestions/q1.java
@@ -0,0 +1,24 @@
+package stringQuestions;
+public class q1{
+
+    public static void reverseString(StringBuilder str){
+        System.out.print("Reverse of "+str+" is: ");
+        for(int i = 0 ; i < str.length(); i++){
+            int front = i;
+            int end = str.length()-1-i;
+
+            char frontChar = str.charAt(front);
+            char endChar = str.charAt(end);
+
+            str.setCharAt(front, endChar);
+            str.setCharAt(end, frontChar);            
+
+        }
+        System.out.print(str);
+        System.out.println();
+    }
+    public static void main(String[]args){
+        StringBuilder str = new StringBuilder("helloNamita");
+        reverseString(str);
+    }
+}
